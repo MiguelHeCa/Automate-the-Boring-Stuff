0e9eecd9f607dcfc1dbecedab3c5d2d60821653a
Mywell <jhernandcab@gmail.com>
2018-06-19 21:58:14 -0500

    Working with multiline comments with escape characters

---
+++ catnapping.py
@@ -0,0 +1,18 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+
+Bob''')
+
+"""This is a text Python program.
+Written by Al Sweigart al@inventwithpython.com
+
+This program was designed for Python 3, not Python 2.
+"""
+
+def spam():
+    """This is a multiline comment to help
+    explaint what the spam() function does."""
+    print('Hello!')
